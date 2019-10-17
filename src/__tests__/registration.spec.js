51bd8fd027bc6861f1a699efa2eaf253a7ce2f62
Nurimansyah Rifwan <nurimansyah.rifwan@tiket.com>
2019-10-17 14:19:08 +0700

    test: added test initial scripts

---
+++ src/__tests__/registration.spec.js
@@ -0,0 +1,10 @@
+import Registration from '../lib/registration';
+
+describe('Registration Unit Test', () => {
+
+    it.todo('Throw an error if payload is empty');
+    it.todo('Throw an error if email is invalid');
+    it.todo('Throw an error if email exist');
+    it.todo('Return success code if user has been registered successfully');
+
+});
